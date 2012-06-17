e91c3e62ee463766a6d973d165158b5919e02fca
xiaodong <xiaodong.yu@irdeto.com>
2012-06-17 22:48:05 +0200

    Initial Commit

---
+++ iostestTests/iostestTests.h
@@ -0,0 +1,13 @@
+//
+//  iostestTests.h
+//  iostestTests
+//
+//  Created by Xiaodong Yu on 17/06/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface iostestTests : SenTestCase
+
+@end
---
+++ iostestTests/iostestTests.m
@@ -0,0 +1,32 @@
+//
+//  iostestTests.m
+//  iostestTests
+//
+//  Created by Xiaodong Yu on 17/06/2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "iostestTests.h"
+
+@implementation iostestTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in iostestTests");
+}
+
+@end
